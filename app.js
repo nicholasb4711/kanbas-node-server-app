bb70b096426f2c0ff203b98caf94c26feaf41daa
nicholasb4711 <nicholasb4711@gmail.com>
2023-12-15 18:39:20 -0500

    part 4

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 import express from 'express';
-import Hello from './Hello.js';
-import Lab5 from './Lab5.js';
+import Hello from './hello.js';
+import Lab5 from './lab5.js';
 import cors from 'cors';
+import CourseRoutes from './courses/routes.js';
 
-const app = express();
-app.use(cors());
-app.use(express.json());
-Hello(app);
-Lab5(app);
-app.listen(4000);
+const App = express();
+App.use(cors());
+App.use(express.json());
+Hello(App);
+Lab5(App);
+CourseRoutes(App);
+App.listen(4000);
