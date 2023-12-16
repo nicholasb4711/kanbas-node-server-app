aa829e020c96fb3cbd5bbd1bfdef1c657f76b07c
nicholasb4711 <nicholasb4711@gmail.com>
2023-12-15 22:50:50 -0500

    a6

--- app.js
+++ app.js
@@ -1,13 +1,18 @@
 import express from "express";
 import "dotenv/config";
 import Hello from "./hello.js";
-import Lab5 from "./lab5.js";
+import Lab5 from "./Lab5.js";
 import CourseRoutes from "./courses/routes.js";
 import ModuleRoutes from "./modules/routes.js";
 import cors from "cors";
+import mongoose from "mongoose";
+import UserRoutes from "./users/routes.js";
+mongoose.connect("mongodb://127.0.0.1:27017/kanbas");
+
 const app = express();
 app.use(cors());
 app.use(express.json());
+UserRoutes(app);
 ModuleRoutes(app);
 CourseRoutes(app);
 Lab5(app);
